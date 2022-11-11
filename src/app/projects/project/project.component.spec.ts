c0bf4de5aaf07ae5c912eca76a51b36609a6e5be
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-11-11 11:11:57 +0100

    Add CI/CD pipeline for frontend project. Fix tests.

--- src/app/projects/project/project.component.spec.ts
+++ src/app/projects/project/project.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProjectComponent } from './project.component';
 
@@ -8,9 +9,9 @@ describe('ProjectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProjectComponent ]
-    })
-    .compileComponents();
+      declarations: [ProjectComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectComponent);
     component = fixture.componentInstance;
