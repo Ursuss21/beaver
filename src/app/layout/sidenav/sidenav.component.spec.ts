8a02baae29d87c3086f49122b0184e3ca89cdfc1
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-11-11 14:17:46 +0100

    Add tests for sidenav component.

--- src/app/layout/sidenav/sidenav.component.spec.ts
+++ src/app/layout/sidenav/sidenav.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LoginComponent } from '../../login/login.component';
 import { LayoutModule } from '../layout.module';
 
 import { SidenavComponent } from './sidenav.component';
@@ -7,19 +9,35 @@ import { SidenavComponent } from './sidenav.component';
 describe('SidenavComponent', () => {
   let component: SidenavComponent;
   let fixture: ComponentFixture<SidenavComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SidenavComponent],
-      imports: [LayoutModule, RouterTestingModule],
+      imports: [
+        LayoutModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'login',
+            component: LoginComponent,
+          },
+        ]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SidenavComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should logout', () => {
+    const routerNavigateSpy = spyOn(router, 'navigateByUrl');
+    component.logout();
+    expect(routerNavigateSpy).toHaveBeenCalledWith('/login');
   });
 });
