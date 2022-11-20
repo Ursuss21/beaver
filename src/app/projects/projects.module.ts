4d645fd483e70a17c105de07469702808d8ee18a
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-11-20 03:09:08 +0100

    Update projects component.

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { ProjectModule } from './project/project.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [CommonModule, ProjectModule, ProjectsRoutingModule],
+  imports: [CommonModule, ProjectModule, ProjectsRoutingModule, SharedModule],
 })
 export class ProjectsModule {}
