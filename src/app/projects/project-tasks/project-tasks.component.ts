5fc3e27c9dd19205c846ae2489e735720a23cc2a
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-11-22 20:06:37 +0100

    Add general styles for tables. Add table to project tasks component.

--- src/app/projects/project-tasks/project-tasks.component.ts
+++ src/app/projects/project-tasks/project-tasks.component.ts
@@ -1,13 +1,27 @@
+import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { Task } from '../model/task.model';
 
 @Component({
   selector: 'bvr-project-tasks',
   templateUrl: './project-tasks.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ButtonComponent, CdkTableModule, CommonModule, FormsModule],
 })
 export class ProjectTasksComponent implements OnInit {
+  dataSource: Task[] = [
+    {
+      name: 'Watering plants',
+    },
+    {
+      name: 'Ironing',
+    },
+  ];
+  displayedColumns: string[] = ['task_name', 'actions'];
+  query: string = '';
   constructor() {}
 
   ngOnInit(): void {}
