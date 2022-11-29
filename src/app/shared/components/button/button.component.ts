a843362864418266ccbcb8b1fd1d295821547eab
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-11-29 13:07:44 +0100

    Work in progress on tracker component.

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -11,6 +11,4 @@ export class ButtonComponent {
   @Input() icon: string = '';
   @Input() label: string = '';
   @Input() styleClass: string = '';
-
-  @Output() onClick: EventEmitter<any> = new EventEmitter();
 }
--- src/app/tracker/tracker.component.ts
+++ src/app/tracker/tracker.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from '../calendar/calendar.component';
+import { ButtonComponent } from '../shared/components/button/button.component';
 
 @Component({
   selector: 'bvr-tracker',
   templateUrl: './tracker.component.html',
   standalone: true,
-  imports: [CalendarComponent, CommonModule],
+  imports: [ButtonComponent, CalendarComponent, CommonModule],
 })
 export class TrackerComponent implements OnInit {
   constructor() {}
