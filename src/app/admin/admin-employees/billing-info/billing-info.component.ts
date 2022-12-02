3e2f03a4147617dd79db2eeb1395ab1f7ee49e7e
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-02 12:36:40 +0100

    Add billing info form.

--- src/app/admin/admin-employees/billing-info/billing-info.component.ts
+++ src/app/admin/admin-employees/billing-info/billing-info.component.ts
@@ -1,18 +1,36 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { RouterModule } from '@angular/router';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
 
 @Component({
   selector: 'bvr-billing-info',
   standalone: true,
-  imports: [ButtonComponent, CommonModule, RouterModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    FormFieldComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './billing-info.component.html',
 })
 export class BillingInfoComponent {
+  @Input() createEmployeeForm!: FormGroup;
+
   @Output() previousStepChange: EventEmitter<void> = new EventEmitter();
 
   previousStep(): void {
     this.previousStepChange.emit();
+  }
+
+  isRequired(name: string): boolean {
+    return this.createEmployeeForm
+      .get(['billingInfo', name])
+      ?.hasValidator(Validators.required)
+      ? true
+      : false;
   }
 }
