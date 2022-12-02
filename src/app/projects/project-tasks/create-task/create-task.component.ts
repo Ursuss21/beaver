13f28e1218425903009a5f25097ddc521c00ba61
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-02 15:32:00 +0100

    Add create task form.

--- src/app/projects/project-tasks/create-task/create-task.component.ts
+++ src/app/projects/project-tasks/create-task/create-task.component.ts
@@ -2,15 +2,51 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
 
 @Component({
   selector: 'bvr-create-task',
   standalone: true,
-  imports: [ButtonComponent, CommonModule, RouterModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    FormFieldComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './create-task.component.html',
 })
 export class CreateTaskComponent {
+  createProjectTaskForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.createProjectTaskForm = this.fb.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    });
+  }
+
   save(): void {
     console.log('saveum');
+  }
+
+  isRequired(name: string): boolean {
+    return this.createProjectTaskForm
+      .get(name)
+      ?.hasValidator(Validators.required)
+      ? true
+      : false;
   }
 }
