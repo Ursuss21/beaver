11d7f11121337aed72a7b911b5da4750f7aa397f
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-02 12:28:09 +0100

    Add contact info form.

--- src/app/admin/admin-employees/contact-info/contact-info.component.ts
+++ src/app/admin/admin-employees/contact-info/contact-info.component.ts
@@ -1,15 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { RouterModule } from '@angular/router';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bvr-contact-info',
   standalone: true,
-  imports: [ButtonComponent, CommonModule, RouterModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    FormFieldComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './contact-info.component.html',
 })
 export class ContactInfoComponent {
+  @Input() createEmployeeForm!: FormGroup;
+
   @Output() nextStepChange: EventEmitter<void> = new EventEmitter();
   @Output() previousStepChange: EventEmitter<void> = new EventEmitter();
 
@@ -19,5 +29,13 @@ export class ContactInfoComponent {
 
   previousStep(): void {
     this.previousStepChange.emit();
+  }
+
+  isRequired(name: string): boolean {
+    return this.createEmployeeForm
+      .get(['contactInfo', name])
+      ?.hasValidator(Validators.required)
+      ? true
+      : false;
   }
 }
