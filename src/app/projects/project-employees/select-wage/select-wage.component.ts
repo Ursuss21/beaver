ea4a9efaec18d82817260d42e7fce81eea44d444
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-11 15:17:08 +0100

    Add content of select-wage component. Rename wrapper to card. Work in progress.

--- src/app/projects/project-employees/select-wage/select-wage.component.ts
+++ src/app/projects/project-employees/select-wage/select-wage.component.ts
@@ -1,18 +1,45 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DropdownListComponent } from '../../../shared/components/dropdown-list/dropdown-list.component';
 
 @Component({
   selector: 'bvr-select-wage',
   standalone: true,
-  imports: [ButtonComponent, CommonModule, RouterModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    DropdownListComponent,
+    FormFieldComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './select-wage.component.html',
 })
 export class SelectWageComponent {
+  @Input() addProjectEmployeeForm!: FormGroup;
+
   @Output() previousStepChange: EventEmitter<void> = new EventEmitter();
+
+  contractTypes: { id: string; name: string }[] = [
+    { id: '1', name: 'Employment contract' },
+    { id: '2', name: 'Commission contract' },
+    { id: '3', name: 'Specific-task contract' },
+    { id: '4', name: 'B2B' },
+  ];
 
   previousStep(): void {
     this.previousStepChange.emit();
+  }
+
+  isRequired(name: string): boolean {
+    return this.addProjectEmployeeForm
+      .get(['employmentInfo', name])
+      ?.hasValidator(Validators.required)
+      ? true
+      : false;
   }
 }
