97ccdeac012072cee8401f91265fe6fcfb8c07ab
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-13 22:33:22 +0100

    Fix build.

--- src/app/admin/admin-employees/admin-employees.component.spec.ts
+++ src/app/admin/admin-employees/admin-employees.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AdminEmployeesComponent } from './admin-employees.component';
@@ -9,7 +10,11 @@ describe('AdminEmployeesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminEmployeesComponent, RouterTestingModule],
+      imports: [
+        AdminEmployeesComponent,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminEmployeesComponent);
