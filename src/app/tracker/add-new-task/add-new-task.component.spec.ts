94b5ae4026996bcd991bd5b96cea2b226dd9f053
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-13 13:59:38 +0100

    Add missing modules to add-new-task test.

--- src/app/tracker/add-new-task/add-new-task.component.spec.ts
+++ src/app/tracker/add-new-task/add-new-task.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AddNewTaskComponent } from './add-new-task.component';
 
@@ -8,7 +10,11 @@ describe('AddNewTaskComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddNewTaskComponent],
+      imports: [
+        AddNewTaskComponent,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddNewTaskComponent);
