34eca3fb91e7d2d2197ed11f77792826c925cf94
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-13 21:31:47 +0100

    Add archive button to view-project-employees.

--- src/app/projects/project-employees/view-project-employee/view-project-employee.component.ts
+++ src/app/projects/project-employees/view-project-employee/view-project-employee.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
 import { ProjectEmployee } from '../../models/project-employee.model';
 import { ProjectEmployeesService } from '../../services/project-employees.service';
 import { ToastComponent } from '../../../shared/components/toast/toast.component';
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
+import { ToastService } from '../../../shared/services/toast.service';
+import { ToastState } from '../../../shared/enum/toast-state';
 
 @Component({
   selector: 'bvr-view-project-employee',
@@ -14,6 +17,7 @@ import { ToastComponent } from '../../../shared/components/toast/toast.component
     ButtonComponent,
     CommonModule,
     FormFieldComponent,
+    ModalComponent,
     RouterModule,
     ToastComponent,
   ],
@@ -35,10 +39,14 @@ export class ViewProjectEmployeeComponent implements OnInit {
     exitDate: '',
     active: true,
   };
+  isArchiveModalOpen: boolean = false;
+  modalDescription: string = '';
 
   constructor(
     private projectEmployeeService: ProjectEmployeesService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
   ) {}
 
   ngOnInit(): void {
@@ -51,5 +59,20 @@ export class ViewProjectEmployeeComponent implements OnInit {
       this.employee =
         this.projectEmployeeService.getProjectEmployee(employeeId);
     }
+  }
+
+  openArchiveModal(): void {
+    this.isArchiveModalOpen = true;
+    this.modalDescription = `Are you sure you want to archive ${this.employee.firstName} ${this.employee.lastName}? This action cannot be undone.`;
+  }
+
+  archive(): void {
+    this.router.navigate(['..'], { relativeTo: this.route }).then(() => {
+      setTimeout(
+        () => this.toastService.showToast(ToastState.Success, 'Task archived'),
+        200
+      );
+      setTimeout(() => this.toastService.dismissToast(), 3200);
+    });
   }
 }
