5bff06565b665288e6b7c3efe2c0f5fe40f26b13
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-15 21:03:19 +0100

    Unsubscribe from approval-tracker tasks to reject subscription on component destroy.

--- src/app/projects/project-approvals/approval-tracker/approval-tracker.component.ts
+++ src/app/projects/project-approvals/approval-tracker/approval-tracker.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { CalendarComponent } from '../../../calendar/calendar.component';
 import { ApprovalTrackerListComponent } from '../approval-tracker-list/approval-tracker-list.component';
 import { EmployeeTask } from '../../../shared/models/employee-task.model';
 import { TasksToRejectService } from '../../../shared/services/tasks-to-reject.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'bvr-approval-tracker',
@@ -17,12 +18,19 @@ import { TasksToRejectService } from '../../../shared/services/tasks-to-reject.s
   ],
   templateUrl: './approval-tracker.component.html',
 })
-export class ApprovalTrackerComponent implements OnInit {
+export class ApprovalTrackerComponent implements OnInit, OnDestroy {
+  private tasksToRejectSubscribtion: Subscription = new Subscription();
+
   constructor(private tasksToRejectService: TasksToRejectService) {}
 
   ngOnInit(): void {
-    this.tasksToRejectService.tasksToReject.subscribe(tasks => {
-      console.log(tasks);
-    });
+    this.tasksToRejectSubscribtion =
+      this.tasksToRejectService.tasksToReject.subscribe(tasks => {
+        console.log(tasks);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.tasksToRejectSubscribtion.unsubscribe();
   }
 }
