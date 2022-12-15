5497b889415b81739b2187afb395725bb7f32f62
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-15 23:41:00 +0100

    Add employee dropdown in approval tracker.

--- src/app/projects/project-approvals/approval-tracker/approval-tracker.component.ts
+++ src/app/projects/project-approvals/approval-tracker/approval-tracker.component.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { CalendarComponent } from '../../../calendar/calendar.component';
 import { ApprovalTrackerListComponent } from '../approval-tracker-list/approval-tracker-list.component';
-import { EmployeeTask } from '../../../shared/models/employee-task.model';
 import { TasksToRejectService } from '../../../shared/services/tasks-to-reject.service';
 import { Subscription } from 'rxjs';
+import { DropdownSearchEmployeeComponent } from '../../project-employees/dropdown-search-employee/dropdown-search-employee.component';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
+import { ProjectEmployeesService } from '../../services/project-employees.service';
+import { Employee } from '../../../shared/models/employee.model';
 
 @Component({
   selector: 'bvr-approval-tracker',
@@ -15,19 +18,33 @@ import { Subscription } from 'rxjs';
     ButtonComponent,
     CalendarComponent,
     CommonModule,
+    DropdownSearchEmployeeComponent,
+    FormFieldComponent,
   ],
   templateUrl: './approval-tracker.component.html',
 })
 export class ApprovalTrackerComponent implements OnInit, OnDestroy {
+  employees: Employee[] = [];
+
   private tasksToRejectSubscribtion: Subscription = new Subscription();
 
-  constructor(private tasksToRejectService: TasksToRejectService) {}
+  constructor(
+    private projectEmployeesService: ProjectEmployeesService,
+    private tasksToRejectService: TasksToRejectService
+  ) {}
 
   ngOnInit(): void {
     this.tasksToRejectSubscribtion =
       this.tasksToRejectService.tasksToReject.subscribe(tasks => {
         console.log(tasks);
       });
+    this.getProjectEmployees();
+  }
+
+  getProjectEmployees(): void {
+    this.employees = this.projectEmployeesService
+      .getProjectEmployees()
+      .map(projectEmployee => projectEmployee.employee);
   }
 
   ngOnDestroy(): void {
