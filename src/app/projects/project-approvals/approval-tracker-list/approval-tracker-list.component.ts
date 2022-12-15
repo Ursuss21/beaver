836713a009b7e82f22f9123dd1e84d5388d58914
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-15 16:42:45 +0100

    Plug in task list to approval-tracker-list.

--- src/app/projects/project-approvals/approval-tracker-list/approval-tracker-list.component.ts
+++ src/app/projects/project-approvals/approval-tracker-list/approval-tracker-list.component.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeTasksComponent } from '../../../shared/components/employee-tasks/employee-tasks.component';
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
+import { ToastComponent } from '../../../shared/components/toast/toast.component';
+import { EmployeeTask } from '../../../shared/models/employee-task.model';
+import { EmployeeProjectTask } from '../../../shared/models/employee-project-task.model';
+import { EmployeeTasksService } from '../../../shared/services/employee-tasks.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from '../../../shared/services/toast.service';
+import { ToastState } from '../../../shared/enum/toast-state';
 
 @Component({
   selector: 'bvr-approval-tracker-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    EmployeeTasksComponent,
+    ModalComponent,
+    ToastComponent,
+  ],
   templateUrl: './approval-tracker-list.component.html',
-  styles: [
-  ]
 })
 export class ApprovalTrackerListComponent {
+  employeeTasks: EmployeeTask[] = [];
+  employeeProjectTasks: EmployeeProjectTask[] = [];
+  isDeleteModalOpen: boolean = false;
+  modalDescription: string = '';
 
+  constructor(
+    private employeeTasksService: EmployeeTasksService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeTasks();
+    this.getEmployeeProjectTasks();
+    this.sortByProjectName();
+  }
+
+  getEmployeeTasks(): void {
+    this.employeeTasks = this.employeeTasksService.getEmployeeTasks();
+  }
+
+  getEmployeeProjectTasks(): void {
+    this.employeeTasks.forEach(employeeTask => {
+      const index = this.findProjectIndex(employeeTask.project.id);
+      if (index !== -1) {
+        this.employeeProjectTasks[index].tasks.push(employeeTask);
+      } else {
+        this.employeeProjectTasks.push({
+          project: employeeTask.project,
+          tasks: [employeeTask],
+        });
+      }
+    });
+  }
+
+  findProjectIndex(projectId: string) {
+    return this.employeeProjectTasks.findIndex(
+      employeeProjectTask => employeeProjectTask.project.id === projectId
+    );
+  }
+
+  sortByProjectName(): void {
+    this.employeeProjectTasks.sort((a, b) =>
+      a.project.name.toLowerCase().localeCompare(b.project.name.toLowerCase())
+    );
+  }
 }
