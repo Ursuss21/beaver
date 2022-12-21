3e30073fd57ed05df4a8b5a8b2ddfb69fdc4f044
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-21 13:20:47 +0100

    Add create-position content.

--- src/app/admin/admin-positions/create-position/create-position.component.spec.ts
+++ src/app/admin/admin-positions/create-position/create-position.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { CreatePositionComponent } from './create-position.component';
@@ -9,7 +10,11 @@ describe('CreatePositionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreatePositionComponent, RouterTestingModule],
+      imports: [
+        BrowserAnimationsModule,
+        CreatePositionComponent,
+        RouterTestingModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreatePositionComponent);
--- src/app/admin/admin-positions/create-position/create-position.component.ts
+++ src/app/admin/admin-positions/create-position/create-position.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
 import {
   FormBuilder,
@@ -9,6 +9,10 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { ToastState } from '../../../shared/enum/toast-state';
+import { ToastService } from '../../../shared/services/toast.service';
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
+import { ToastComponent } from '../../../shared/components/toast/toast.component';
 
 @Component({
   selector: 'bvr-create-position',
@@ -17,15 +21,26 @@ import {
     ButtonComponent,
     CommonModule,
     FormFieldComponent,
+    ModalComponent,
     ReactiveFormsModule,
     RouterModule,
+    ToastComponent,
   ],
   templateUrl: './create-position.component.html',
 })
 export class CreatePositionComponent implements OnInit {
   createPositionForm!: FormGroup;
+  isAddModalOpen: boolean = false;
+  isCancelModalOpen: boolean = false;
+  modalDescription: string = '';
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private location: Location,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit(): void {
     this.createForm();
@@ -38,13 +53,58 @@ export class CreatePositionComponent implements OnInit {
     });
   }
 
-  save(): void {
-    console.log('saveum');
+  openAddModal(): void {
+    if (this.createPositionForm.valid) {
+      const name = this.createPositionForm.get('name')?.value;
+      this.isAddModalOpen = true;
+      this.modalDescription = `Are you sure you want to add ${name}?`;
+    } else {
+      this.createPositionForm.markAllAsTouched();
+      this.toastService.showToast(ToastState.Error, 'Form invalid');
+      setTimeout(() => this.toastService.dismissToast(), 3000);
+    }
+  }
+
+  openCancelModal(): void {
+    this.isCancelModalOpen = true;
+    this.modalDescription = `Are you sure you want to leave? You will lose your unsaved changes if you continue.`;
+  }
+
+  add(): void {
+    this.router.navigate(['..'], { relativeTo: this.route }).then(() => {
+      setTimeout(
+        () =>
+          this.toastService.showToast(ToastState.Success, 'Employee created'),
+        200
+      );
+      setTimeout(() => this.toastService.dismissToast(), 3200);
+    });
+  }
+
+  cancel(): void {
+    this.location.back();
   }
 
   isRequired(name: string): boolean {
     return this.createPositionForm.get(name)?.hasValidator(Validators.required)
       ? true
       : false;
+  }
+
+  showErrors(name?: string): boolean {
+    if (name) {
+      return !!(
+        this.createPositionForm.get(name)?.invalid &&
+        this.createPositionForm.get(name)?.errors &&
+        (this.createPositionForm.get(name)?.dirty ||
+          this.createPositionForm.get(name)?.touched)
+      );
+    } else {
+      return !!(
+        this.createPositionForm.invalid &&
+        this.createPositionForm.errors &&
+        (this.createPositionForm.dirty || this.createPositionForm.touched)
+      );
+    }
   }
 }
