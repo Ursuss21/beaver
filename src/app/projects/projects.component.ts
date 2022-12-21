f78388c921f6b387832f5282768492dacd03568c
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-21 14:37:27 +0100

    Adjust styles of login page. Dynamically generate project buttons.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { first } from 'rxjs';
 import { ButtonComponent } from '../shared/components/button/button.component';
+import { ProjectsService } from '../shared/services/projects.service';
+import { Project } from './models/project.model';
 
 @Component({
   selector: 'bvr-projects',
@@ -10,7 +13,18 @@ import { ButtonComponent } from '../shared/components/button/button.component';
   imports: [ButtonComponent, CommonModule, RouterModule],
 })
 export class ProjectsComponent implements OnInit {
-  constructor() {}
+  projects: Project[] = [];
 
-  ngOnInit(): void {}
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(): void {
+    this.projectsService
+      .getProjects()
+      .pipe(first())
+      .subscribe(projects => (this.projects = projects));
+  }
 }
