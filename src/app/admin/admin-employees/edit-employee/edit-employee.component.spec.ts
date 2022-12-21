ff9b10f246a48eebd53fcc07316f6ad84a7dfa7a
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-21 10:48:27 +0100

    Fix build.

--- src/app/admin/admin-employees/edit-employee/edit-employee.component.spec.ts
+++ src/app/admin/admin-employees/edit-employee/edit-employee.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditEmployeeComponent } from './edit-employee.component';
@@ -9,7 +10,11 @@ describe('EditEmployeeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditEmployeeComponent, RouterTestingModule],
+      imports: [
+        BrowserAnimationsModule,
+        EditEmployeeComponent,
+        RouterTestingModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EditEmployeeComponent);
--- src/app/admin/admin-employees/view-employee/view-employee.component.spec.ts
+++ src/app/admin/admin-employees/view-employee/view-employee.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ViewEmployeeComponent } from './view-employee.component';
@@ -9,7 +10,11 @@ describe('ViewEmployeeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, ViewEmployeeComponent],
+      imports: [
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        ViewEmployeeComponent,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ViewEmployeeComponent);
