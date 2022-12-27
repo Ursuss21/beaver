fea99baae7cec8547885c6eabf97faf69cabcdcd
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-27 23:58:19 +0100

    Finish view-employee flow.

--- src/app/admin/admin-employees/view-account-info/view-account-info.component.ts
+++ src/app/admin/admin-employees/view-account-info/view-account-info.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Account } from '../../../shared/models/account.model';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
 
 @Component({
   selector: 'bvr-view-account-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormFieldComponent],
   templateUrl: './view-account-info.component.html',
 })
 export class ViewAccountInfoComponent {
