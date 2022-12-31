91adf62aa36209c596b20746e04974ea27b4b023
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-12-31 18:58:44 +0100

    Update login page.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '../shared/components/button/button.component';
 import { AuthService } from '../shared/services/auth.service';
+import { ValidationService } from '../shared/services/validation.service';
 
 @Component({
   selector: 'bvr-login',
@@ -12,24 +13,38 @@ import { AuthService } from '../shared/services/auth.service';
   imports: [ButtonComponent, CommonModule, ReactiveFormsModule],
 })
 export class LoginComponent implements OnInit {
+  isPasswordForgotten: boolean = false;
   loginForm = this.fb.group({
-    email: [''],
-    password: [''],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
   });
 
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private validationService: ValidationService
   ) {}
 
   ngOnInit(): void {}
 
   login(): void {
-    this.authService.login().subscribe(isLoggedIn => {
-      if (isLoggedIn) {
-        this.router.navigateByUrl('/dashboard');
-      }
-    });
+    if (this.loginForm.valid) {
+      this.authService.login().subscribe(isLoggedIn => {
+        if (isLoggedIn) {
+          this.router.navigateByUrl('/dashboard');
+        }
+      });
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  forgotPassword(): void {
+    this.isPasswordForgotten = !this.isPasswordForgotten;
+  }
+
+  showErrors(name: string): boolean {
+    return this.validationService.showErrors(this.loginForm, [name]);
   }
 }
