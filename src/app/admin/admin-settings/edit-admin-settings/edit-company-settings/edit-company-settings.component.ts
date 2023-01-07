9639c5e1dcc40b7f7d237ba24b259bfa823d7636
Ursuss21 <robert.skrzypczak21@gmail.com>
2023-01-07 23:08:41 +0100

    Add content of edit-general-settings.

--- src/app/admin/admin-settings/edit-admin-settings/edit-company-settings/edit-company-settings.component.ts
+++ src/app/admin/admin-settings/edit-admin-settings/edit-company-settings/edit-company-settings.component.ts
@@ -1,13 +1,39 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormFieldComponent } from '../../../../shared/components/form-field/form-field.component';
+import { ValidationService } from '../../../../shared/services/validation.service';
+import { FileUploadComponent } from '../../../../shared/components/file-upload/file-upload.component';
 
 @Component({
   selector: 'bvr-edit-company-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FileUploadComponent,
+    FormFieldComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './edit-company-settings.component.html',
 })
 export class EditCompanySettingsComponent {
   @Input() editGlobalSettingsForm!: FormGroup;
+
+  constructor(private validationService: ValidationService) {}
+
+  isRequired(name: string): boolean {
+    return this.validationService.isRequired(this.editGlobalSettingsForm, [
+      'companyInfo',
+      name,
+    ]);
+  }
+
+  showErrors(name?: string): boolean {
+    return name
+      ? this.validationService.showErrors(this.editGlobalSettingsForm, [
+          'companyInfo',
+          name,
+        ])
+      : this.validationService.showErrors(this.editGlobalSettingsForm, []);
+  }
 }
