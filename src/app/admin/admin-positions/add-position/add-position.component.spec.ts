2138314f038670bb74392647856d19dbcffe848e
Ursuss21 <robert.skrzypczak21@gmail.com>
2023-01-10 13:31:25 +0100

    Fix build.

--- src/app/admin/admin-positions/add-position/add-position.component.spec.ts
+++ src/app/admin/admin-positions/add-position/add-position.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -11,8 +12,9 @@ describe('AddPositionComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        NoopAnimationsModule,
         AddPositionComponent,
+        HttpClientTestingModule,
+        NoopAnimationsModule,
         RouterTestingModule,
       ],
     }).compileComponents();
