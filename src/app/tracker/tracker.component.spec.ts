2ed76d1b605f6108114a99e16addc78afe7e32ea
Ursuss21 <robert.skrzypczak21@gmail.com>
2023-01-12 11:00:54 +0100

    Fix build.

--- src/app/tracker/tracker.component.spec.ts
+++ src/app/tracker/tracker.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CalendarComponent } from '../calendar/calendar.component';
@@ -10,7 +11,11 @@ describe('TrackerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CalendarComponent, RouterTestingModule],
+      imports: [
+        CalendarComponent,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TrackerComponent);
